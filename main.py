abf4b324697ad1e8c98c3509ef319124a63a1a5e
pebabion <28013335+pebabion@users.noreply.github.com>
2025-09-03 01:54:00 -0400

    feat: data analyst agent

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
+import asyncio
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_tavily import TavilySearch
 from langgraph.prebuilt import create_react_agent
 
-tavily_search = TavilySearch(
-    max_results=3, description="Search the internet for information using Tavily"
-)
+
+async def main():
+    tavily_search = TavilySearch(
+        max_results=3, description="Search the internet for information using Tavily"
+    )
+
+    client = MultiServerMCPClient(
+        {
+            "postgres": {
+                "command": "npx",
+                "args": [
+                    "-y",
+                    "@modelcontextprotocol/server-postgres",
+                    "postgresql://postgres:secret@localhost:54321/Adventureworks",
+                ],
+                "transport": "stdio",
+            },
+        }
+    )
+
+    mcp_tools = await client.get_tools()
+    tools = mcp_tools + [tavily_search]
+
+    agent = create_react_agent(
+        model="anthropic:claude-3-7-sonnet-latest",
+        tools=tools,
+        prompt="""
+        You are a helpful assistant that can answer questions about the data in the database or the internet.
+        If you need to query the database, you should be looking into the `Adventureworks` database, `sales` schema.
+        """,
+    )
+
+    # Use astream() for async streaming responses
+    async for chunk in agent.astream(
+        {"messages": [{"role": "user", "content": "What is  the total sales in 2012?"}]}
+    ):
+        print(chunk)
+        print("---" * 20)
 
 
-agent = create_react_agent(
-    model="anthropic:claude-3-7-sonnet-latest",
-    tools=[tavily_search],
-    prompt="You are a helpful assistant",
-)
-
-
-response = agent.invoke(
-    {
-        "messages": [
-            {
-                "role": "user",
-                "content": "what is the date today and what is the weather in sf",
-            }
-        ]
-    }
-)
-
-for message in response["messages"]:
-    message.pretty_print()
+# Run the async main function
+if __name__ == "__main__":
+    asyncio.run(main())
