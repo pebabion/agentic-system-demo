526c7a62c6a19069e5294ec50b4e75df9f76fe53
pebabion <28013335+pebabion@users.noreply.github.com>
2025-09-03 01:14:44 -0400

    feat: agent and tools

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from agentic-system-demo!")
+from langgraph.prebuilt import create_react_agent
 
 
-if __name__ == "__main__":
-    main()
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+
+agent = create_react_agent(
+    model="anthropic:claude-3-7-sonnet-latest",
+    tools=[get_weather],
+    prompt="You are a helpful assistant",
+)
+
+
+response = agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+
+for message in response["messages"]:
+    message.pretty_print()
